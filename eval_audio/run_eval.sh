04ea8cd7d7528a2c4d059b425f3f6dfbc7549921
Yuantao Feng <yuantao.feng@outlook.com>
2025-08-27 11:50:16 +0800

    feat: add script to run eval

---
+++ eval_audio/run_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+workspace_dir=/workspace
+checkpoint=${workspace_dir}/models/Qwen2.5-Omni-7B
+#for ds in "librispeech" "aishell2" "cv15_en" "cv15_zh" "cv15_yue" "cv15_fr" "fluers_zh"
+for ds in "${workspace_dir}/datasets/fluers_zh"
+do
+    python -m torch.distributed.launch --use_env \
+        --nproc_per_node ${NPROC_PER_NODE:-8} --nnodes 1 \
+        evaluate_asr.py \
+        --checkpoint $checkpoint \
+        --dataset $ds \
+        --batch-size 20 \
+        --num-workers 2
+done
